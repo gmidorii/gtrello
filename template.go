1004984081b54b882b1d78cab7aa83e806f83133
tagasouichiro <stitow14@gmail.com>
2017-10-02 23:19:19 +0900

    Update output

--- template.go
+++ template.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"os"
+	"time"
 )
+
+const format = "2006-01-02"
 
 func outputFile(file string, output Output, outputPath string) error {
 	tmp, err := template.New("template.md").ParseFiles(file)
 	if err != nil {
 		return err
 	}
-	foutput, err := os.Create(outputPath)
+	now := time.Now()
+	foutput, err := os.Create(fmt.Sprintf("%s-%s", now.Format(format), outputPath))
 	if err != nil {
 		return err
 	}
